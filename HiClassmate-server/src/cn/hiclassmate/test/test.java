c7b5d1fb1c53e228d84b8524acff90e01dc17f30
howei.s <howei.s@foxmail.com>
2019-09-15 23:35:04 +0800

    initial project

---
+++ HiClassmate-server/src/cn/hiclassmate/test/test.java
@@ -0,0 +1,13 @@
+package cn.hiclassmate.test;
+
+/**
+ * @author hessler
+ * @date 2019/9/15 - 23:26
+ */
+
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello HiClassmate");
+    }
+}
